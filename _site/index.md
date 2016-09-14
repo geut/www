98b07d560b2b62a1f85687f56946e7634ac9a6c4
Maximiliano Fierro <elmasse@gmail.com>
2016-09-13 21:19:45 -0300

    resources endpoint. Added inert back. First main.css

--- _site/index.md
+++ _site/index.md
@@ -1,2 +1,2 @@
-# Index
+# Index**Geut**
 This is the `index` file
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ import router from '../lib/router';
 
 exports.register = (server, options, next) => {
     const { GET } = router(server);
-    const home = { handler: { markdown: '_site/index' } };
+
+    const home  = { handler: { markdown: '_site/index' } };
     const about = { handler: { markdown: '_site/about' } };
+    const resources = { handler: { directory: { path: '_resources' } } };
 
-    GET `/          ${home}`;
-    GET `/about     ${about}`;
+    GET `/resources/{v*} ${resources}`; // public resources (css, images, etc)
 
+    GET `/               ${home}  `;
+    GET `/about          ${about} `;
+    
     next();
 };
 
