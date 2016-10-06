15891098623c9c609db12c4601bbf41f92aad538
tincho <tinchoz49@gmail.com>
2016-10-06 19:22:33 -0300

    removed span word

--- _resources/_browserify/components/title/index.js
+++ _resources/_browserify/components/title/index.js
@@ -2,26 +2,12 @@ import velocity from 'velocity-animate';
 
 export default function draw() {
     const element = document.querySelector('#title');
-    const geut = element.querySelector('.geut .word');
-    const studio = element.querySelector('.studio');
 
-    const show = {
-        o: {
-            easing: 'easeInOutQuad',
-            duration: 500,
-            sequenceQueue: false
-        },
-        p: {
-            opacity: 1
-        }
-    };
-
-    const sequence = [
-        { e: geut, o: {delay: 300, ...show.o}, p: show.p },
-        { e: studio, o: {delay: 300, ...show.o}, p: show.p }
-    ];
-
-    return Promise.all(
-        sequence.map((anim) => velocity(anim.e, anim.p, anim.o))
-    );
+    return velocity(element, {
+        opacity: 1
+    }, {
+        easing: 'easeInOutQuad',
+        duration: 500,
+        sequenceQueue: false
+    });
 }
