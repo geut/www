79cf57729c16be203b48bee5f252321d54f5889f
tincho <tinchoz49@gmail.com>
2016-10-19 17:26:35 -0300

    changed about us, refactory default css

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,15 @@ import router from '../lib/router';
 export const register = (server, options, next) => {
     const { GET } = router(server);
 
-    const home      = { handler: { markdown: { template: 'main.html', sections:['philosophy.md', 'section.md'] } } };
-    const about     = { handler: { markdown: { template: 'section.html', path: 'about.md' } } };
-    const section   = { handler: { markdown: { template: 'section.html', path: 'section.md' } } };
+    const home      = { handler: { markdown: { template: 'main.html', sections:['philosophy.md', 'about.md', 'section.md'] } } };
+    // const about     = { handler: { markdown: { template: 'section.html', path: 'about.md' } } };
+    // const section   = { handler: { markdown: { template: 'section.html', path: 'section.md' } } };
 
     const resources = { handler: { directory: { path: '_resources' } } };
 
     GET `/        ${home}`;
-    GET `/about   ${about}`;
-    GET `/section ${section}`;
+    // GET `/about   ${about}`;
+    // GET `/section ${section}`;
 
     // public resources (css, images, etc)
     GET `/resources/{path*} ${resources}`;
