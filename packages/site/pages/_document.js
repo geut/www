10487106c967b5167c04a00b50b2daffe3fe672d
Max Fierro <elmasse@gmail.com>
2019-01-13 22:39:13 -0300

    Add favicon.

--- packages/site/pages/_document.js
+++ packages/site/pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <link href="./static/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="./static/css/gridlex.min.css" media="all" rel="stylesheet" type="text/css" />
           <link href="./static/css/btns.min.css" media="all" rel="stylesheet" type="text/css" />
