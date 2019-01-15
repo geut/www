d36bc62f538dcf2db9b00578c170e1f53e2328c4
Max Fierro <elmasse@gmail.com>
2019-01-14 23:54:32 -0300

    api: contact mailer.

--- packages/contact/index.js
+++ packages/contact/index.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto')
-const { json } = require('micro')
+const { json, send, sendError } = require('micro')
 const nodemailer = require('nodemailer')
 const yup = require('yup')
 
@@ -10,60 +10,37 @@ const schema = yup.object().shape({
   comment: yup.string().required()
 })
 
-const transporter = process.env.MAIL_HOST ? nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE, // use SSL
+const transporter = nodemailer.createTransport({
+  service: 'Zoho',
   auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD
   }
-}) : false;
+});
 
 module.exports = async (request, response) => {
   const value = await json(request)
-  const data =  await schema.validate(value)
 
-  await transporter.sendMail({
-    from: `Geut Studio Website <${process.env.MAIL_USER}>`,
-    to: process.env.MAIL_TO,
-    subject: `Contact from the website - ${crypto.createHash('sha1').update(data.comment).digest('hex').substr(0, 16)}`,
-    html: `
-    <b>Name: </b> ${data.name} <br/>
-    <b>Email: </b> ${data.email} <br/>
-    <b>Company: </b> ${data.company} <br/>
-    <b>Comment: </b> ${data.comment}
-    `
-  })
+  try {  
+    const { name, email, company = 'N/A', comment } =  await schema.validate(value)
 
-  return {
-    status: 'success'
+    await transporter.sendMail({
+      from: `Geut Studio Website <${process.env.MAIL_USER}>`,
+      to: process.env.MAIL_TO,
+      subject: `Contact from the website - ${crypto.createHash('sha1').update(comment).digest('hex').substr(0, 16)}`,
+      html: `
+      <b>Name: </b> ${name} <br/>
+      <b>Email: </b> ${email} <br/>
+      <b>Company: </b> ${company} <br/>
+      <b>Comment: </b></br>
+      ${comment}
+      `
+    })
+
+    send(response, 200, { status: 'success' })
+
+  } catch (err) {
+    send(response,  500, { status: 'failed', info: err })
   }
-  // transporter.sendMail({
-  //   from: `Geut Studio Website <${process.env.MAIL_USER}>`,
-  //   to: process.env.MAIL_TO,
-  //   subject: `Contact from the website - ${crypto.createHash('sha1').update(request.payload.comment).digest('hex').substr(0, 16)}`,
-  //   html: `
-  //   <b>Name: </b> ${request.payload.name} <br/>
-  //   <b>Email: </b> ${request.payload.email} <br/>
-  //   <b>Company: </b> ${request.payload.company} <br/>
-  //   <b>Comment: </b> ${request.payload.comment}
-  //   `
-  // }, (error) => {
-  //   if (error) {
-  //     console.log('error', {
-  //       error,
-  //       ...request
-  //     });
-  //     response.send({
-  //       status: 'error',
-  //       message: 'error sending the email'
-  //     });
-  //   } else {
-  //     console.log('info', request);
-  //     response.send({
-  //       status: 'success'
-  //     });
-  //   }
-  // });
+
 }
