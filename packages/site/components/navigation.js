1d1433c0991a2e059cbcd2d790380770fb1951f7
Max Fierro <elmasse@gmail.com>
2019-01-21 00:11:56 -0300

    Add navigation animation.

--- packages/site/components/navigation.js
+++ packages/site/components/navigation.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import anime from 'animejs'
+
+const scrollAnimated = (event) => {
+  const { target } = event
+  const { hash } = new URL(target.href)
+  
+  const refId = hash.replace('#', '')
+
+  const targetSection = document.querySelector(`[id=${refId}]`)
+  if (targetSection) {
+    event.preventDefault()
+    const { top } = targetSection.getBoundingClientRect()
+    const scrollCoords = {
+      y: window.pageYOffset
+    }
+  
+    anime({
+      targets: scrollCoords,
+      y: ((top + window.pageYOffset)),
+      duration: 350,
+      easing: 'easeInOutQuad',
+      update: () => {
+        window.location.hash = hash
+        window.scroll(0, scrollCoords.y)
+      }
+    })    
+  }
+}
 
 export default React.forwardRef((props, ref) => (
   <nav role="navigation" {...props} ref={ref}>
     <ul>
-      <li><a href="#our-mission">Mission</a></li>
-      <li><a href="#about-us">About Us</a></li>
-      <li><a href="#contact-us">Contact</a></li>
+      <li><a href="#our-mission" onClick={scrollAnimated}>Mission</a></li>
+      <li><a href="#about-us" onClick={scrollAnimated}>About Us</a></li>
+      <li><a href="#contact-us" onClick={scrollAnimated}>Contact</a></li>
     </ul>
     <style jsx>{`
       nav li {
