026f48b1f1536f08d77bfb1aa740d84b3dcee09f
Diego Paez <diego@geutstudio.com>
2020-11-11 18:21:10 -0300

    Update: move viewport declaration here

--- packages/site/components/meta.js
+++ packages/site/components/meta.js
@@ -9,8 +9,10 @@ export default class Meta extends Component {
     return (
       <Head>
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
+        <meta property="og:url" content={url} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={`${url}/static/images/logo.png`} />
       </Head>
